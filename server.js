79fed254e6ec550c1145cf6713fed7071e1407d7
Premapriya1905 <Premapriya1905@gmail.com>
2024-02-20 17:02:48 +0530

    completed

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+require("dotenv").config();
+
 const express =require("express")
 const app=express()
+const mongoose=require("mongoose")
+const {connectDB,isConnectedNow}=require('./config/dbConn.js')
+
+
+
 
 app.get('/ping', (req,res) =>{
-    res.send('Hello NODE API')
+    res.send('Hello')
 })
-
-app.listen(3000, ()=>{
-
-    console.log('Node API app is running on port 3000')
+app.get('/home', (req,res) =>{
+    res.json({
+        message: isConnectedNow()?"Database is connected":"Database is disconnected"
+    })
 })
+  
+app.listen(1000, async()=>{
+    await connectDB();
+    console.log('Server is running on port 3000')
+});
