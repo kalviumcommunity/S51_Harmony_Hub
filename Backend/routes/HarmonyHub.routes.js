7fe0639f0470e857e0bd914e261bbc9935b9dc04
Premapriya1905 <Premapriya1905@gmail.com>
2024-02-23 16:50:24 +0530

    Updating backend

---
+++ Backend/routes/HarmonyHub.routes.js
@@ -0,0 +1,91 @@
+const express = require('express')
+
+const getRouter = express.Router();
+const postRouter = express.Router();
+const putRouter = express.Router();
+const deleteRouter = express.Router();
+const HarmonyHub = require("../Backend/Model/HarmonyHub.model");
+
+getRouter.get('/getallharmonyhub', async (req, res) => {
+    try {
+        const harmonyHubs = await HarmonyHub.find();
+        res.status(200).json(harmonyHubs);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({
+            message: "Internal server error"
+        });
+    }
+});
+
+getRouter.get('/getharmonyhub/:id', async (req, res) => {
+    try {
+        const harmonyHub = await HarmonyHub.findOne({ deviceId: req.params.id });
+        if (!harmonyHub) {
+            return res.status(404).send({
+                message: "Harmony Hub not found"
+            });
+        }
+        res.status(200).json(harmonyHub);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({
+            message: "Internal server error"
+        });
+    }
+});
+
+postRouter.post('/addharmonyhub', async (req, res) => {
+    try {
+        const {ID,NAME,EMAIL,MOVIENAME,SONGNAME,SONGLINK,LYRICSLINK,ARTIST} = req.body
+        const newHarmonyHub = await HarmonyHub.create({ID,NAME,EMAIL,MOVIENAME,SONGNAME,SONGLINK,LYRICSLINK,ARTIST});
+        res.status(201).json(newHarmonyHub);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({
+            message: "Internal server error"
+        });
+    }
+});
+
+putRouter.patch('/updateharmonyhub/:id', async (req, res) => {
+    try {
+        const harmonyHubId = req.params.id;
+        const updateData = req.body;
+
+        const updatedHarmonyHub = await HarmonyHub.findOneAndUpdate(
+            { ID: harmonyHubId },
+            { $set: updateData },
+            { new: true }
+        );
+
+        if (!updatedHarmonyHub) {
+            return res.status(404).send({
+                message: "Harmony Hub not found"
+            });
+        }
+
+        res.status(200).json(updatedHarmonyHub);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({
+            message: "Internal server error"
+        });
+    }
+});
+
+
+deleteRouter.delete('/deleteharmonyhub/:id', async (req, res) => {
+    try {
+        const harmonyHubId = req.params.id;
+        const deletedHarmonyHub = await HarmonyHub.findOneAndDelete({harmonyHubId});     
+        res.status(200).json("deleted HarmonyHub");
+    } catch (err) {
+        console.log(err);
+        return res.status(500).send({
+            message: "Internal server error"
+        });
+    }
+});
+
+module.exports = { getRouter, postRouter, deleteRouter, putRouter };
---
+++ Backend/server.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const bodyParser=require('body-parser')
+const express =require("express")
+const app=express()
+const mongoose=require("mongoose")
+const {connectDB,isConnectedNow}=require('./config/dbConn.js')
+
+const { getRouter, postRouter, deleteRouter, putRouter }=require("../routes/HarmonyHub.routes.js")
+app.use(bodyParser.json())
+app.use("/",getRouter)
+app.use("/",postRouter)
+app.use("/",deleteRouter)
+app.use("/",putRouter)
+
+
+
+app.get('/ping', (req,res) =>{
+    res.send('Hello')
+})
+app.get('/home', (req,res) =>{
+    res.json({
+        message: isConnectedNow()?"Database is connected":"Database is disconnected"
+    })
+})
+  
+app.listen(3000, async()=>{
+    await connectDB();
+    console.log('Server is running on port 3000')
+});
